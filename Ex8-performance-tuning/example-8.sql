4e78633e8f70ceb2450cd3fcf4439f3f70699e1a
Brijesh Jaggi <brijesh.jaggi@gmail.com>
2022-08-31 16:55:03 -0400

    performance tuning

--- Ex8-performance-tuning/example-8.sql
+++ Ex8-performance-tuning/example-8.sql
@@ -3,4 +3,8 @@ At high level the most important tuning suggestions are
 1. Increase number or partitions 
 2. Increase number of tasks ( On cloud ) 
 
+For a more detailed guide refer this document.
+
+https://docs.ksqldb.io/en/latest/operate-and-deploy/performance-guidelines/
+
 
